e4f0d60cdcd96e4f346c18b53cdb98c8361406eb
Karuna <Karuna28396@gmail.com>
2017-07-15 17:11:01 +0530

    learn mvc

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// run when html is loaded
+var foodieApp = angular.module('foodieApp',[]);
+foodieApp.controller('mainController',function($scope) {
+  $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+})
